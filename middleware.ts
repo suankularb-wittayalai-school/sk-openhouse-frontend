4030b055157afc2d3c421f2f06a9f3efe7db601a
Sun-52 <iamsun2552@gmail.com>
2025-12-27 23:20:25 +0700

    feat: initialize middleware and localization

---
+++ middleware.ts
@@ -0,0 +1,43 @@
+// Imports
+import { NextRequest, NextResponse } from "next/server";
+import { LangCode } from "@/utils/types/common";
+import getLocalePath from "@/utils/helpers/getLocalePaths";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  // Get original destination
+  const route = req.nextUrl.pathname;
+  const locale = req.nextUrl.locale as LangCode;
+
+  // Log middleware start
+  if (process.env.NODE_ENV === "development")
+    console.log(
+      `\u001b[1m\x1b[35m →\u001b[0m Running middleware on ${route} …`
+    );
+
+  // Get current page protection type
+  const pageRole = (() => {
+    return "user";
+  })();
+
+  // Decide on destination based on user and page protection type
+  const destination = (() => {
+    return null;
+  })();
+
+  // Log middleware end
+  if (process.env.NODE_ENV === "development")
+    console.log(
+      `\u001b[1m\x1b[35m →\x1b[0m\u001b[0m ${
+        destination ? `Redirected to ${destination}` : "Continued"
+      }`
+    );
+
+  // Redirect if decided so, continue if not
+  if (destination)
+    return NextResponse.redirect(
+      new URL(getLocalePath(destination, locale), req.url)
+    );
+  return NextResponse.next();
+}
---
+++ utils/types/common.ts
@@ -0,0 +1,4 @@
+/**
+ * The language code of a supported UI language.
+ */
+export type LangCode = "en-US" | "th";

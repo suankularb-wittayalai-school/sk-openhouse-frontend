ce493ecdef5f57f82b954ff9ba1adc70bbdf245a
Sun-52 <iamsun2552@gmail.com>
2025-12-29 15:46:48 +0700

    feat: Card Component

---
+++ components/common/Card.tsx
@@ -0,0 +1,23 @@
+import cn from "@/utils/helpers/cn";
+import { StylableFC } from "@/utils/types/common";
+import React from "react";
+
+const Card: StylableFC<{ children: React.ReactNode }> = ({
+  children,
+  style,
+  className,
+}) => {
+  return (
+    <div
+      style={style}
+      className={cn(
+        "bg-on-background border-primary-border flex flex-col gap-2 rounded-lg border p-3",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;

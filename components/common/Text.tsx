dc8a661469246092e79022e4a1db9550b3ba8c6d
Sun-52 <iamsun2552@gmail.com>
2025-12-29 14:10:13 +0700

    feat: Text Component

---
+++ components/common/Text.tsx
@@ -0,0 +1,23 @@
+// Imports
+import { StylableFC } from "@/utils/types/common";
+import cn from "@/utils/helpers/cn";
+import { JSX } from "react";
+
+const Text: StylableFC<{
+  type: "headline" | "title" | "body";
+  element?: keyof JSX.IntrinsicElements;
+  children: React.ReactNode;
+}> = ({ type, element: Element = "span", children, style, className }) => {
+  const TYPE_STYLE = {
+    headline: "text-primary text-2xl font-bold leading-[120%]",
+    title: "text-primary text-sm font-normal leading-[140%]",
+    body: "text-tertiary text-xs font-normal leading-[140%] opacity-",
+  };
+  return (
+    <Element style={style} className={cn(TYPE_STYLE[type], className)}>
+      {children}
+    </Element>
+  );
+};
+
+export default Text;

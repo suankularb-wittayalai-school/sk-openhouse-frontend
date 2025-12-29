1f39a23bac30ce6ef88b1cf1e0c52f124d20cc84
Sun-52 <iamsun2552@gmail.com>
2025-12-29 13:31:43 +0700

    feat: StylableFC Type

--- utils/types/common.ts
+++ utils/types/common.ts
@@ -1,4 +1,14 @@
+// Imports
+import { CSSProperties, FC } from "react";
+
 /**
  * The language code of a supported UI language.
  */
 export type LangCode = "en-US" | "th";
+
+/**
+ * A function component stylable through `className` and `style`.
+ */
+export type StylableFC<Props extends {} = {}> = FC<
+  Props & { className?: string; style?: CSSProperties }
+>;

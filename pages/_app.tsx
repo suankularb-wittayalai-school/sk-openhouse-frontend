549e208a8ee432793ebcf0012606d72c53b450ad
Nubwan <149947073+Nubboyy@users.noreply.github.com>
2025-12-29 21:50:34 +0700

    fix: font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,25 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextIntlClientProvider } from "next-intl";
 import { useRouter } from "next/router";
+import Header from "@/components/common/Header";
+import localFont from "next/font/local";
+import cn from "@/utils/helpers/cn";
+
+const lineSeed = localFont({
+    src: [
+      {
+        path: "../public/fonts/LINESeedSansTH_W_Rg.woff2",
+        weight: "400",
+        style: "normal",
+      },
+      {
+        path: "../public/fonts/LINESeedSansTH_W_Bd.woff2",
+        weight: "700",
+        style: "normal",
+      },
+    ],
+    variable: "--font-line-seed",
+  });
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -11,7 +30,16 @@ function App({ Component, pageProps }: AppProps) {
       timeZone="Asia/Bangkok"
       messages={pageProps.messages}
     >
-      <Component {...pageProps} />
+      <div
+        className={cn(
+          lineSeed.variable,
+          "font-line-seed",
+          "max-w-content-max mx-auto p-4",
+        )}
+      >
+        <Header />
+        <Component {...pageProps} />
+      </div>
     </NextIntlClientProvider>
   );
 }

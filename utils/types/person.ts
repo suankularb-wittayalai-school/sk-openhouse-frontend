8b9084945cbdd9b3f0a2ce1f7fd5bd6a3ca3baed
Sun-52 <iamsun2552@gmail.com>
2025-12-29 22:19:55 +0700

    fix: mistyped prefix enum

---
+++ utils/types/person.ts
@@ -0,0 +1,41 @@
+/**
+ * The prefix of a person.
+ */
+export enum prefix {
+  master = "master",
+  mr = "mr",
+  miss = "miss",
+  ms = "ms",
+  mrs = "mrs",
+}
+
+/**
+ * The gender of a person.
+ */
+export enum gender {
+  male = "male",
+  female = "female",
+  other = "other",
+}
+
+/**
+ * The relationship of a person to his / her child(ren).
+ */
+export enum relationshipToChild {
+  father = "father",
+  mother = "mother",
+  legal_guardian = "legal_guardian",
+  other = "other",
+}
+
+export type person = {
+  id: string;
+  prefix: prefix;
+  firstname: string;
+  lastname: string;
+  birthdate: Date;
+  gender: gender;
+  tel: string;
+  is_child: boolean;
+  relationship_to_child: relationshipToChild;
+};

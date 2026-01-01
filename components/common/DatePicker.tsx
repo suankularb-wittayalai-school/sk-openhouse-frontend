7b03db02d6360a3c3356b39d218db8e4ac9fa459
Sun-52 <iamsun2552@gmail.com>
2026-01-01 17:31:44 +0700

    fix: DatePicker Component styling

--- components/common/DatePicker.tsx
+++ components/common/DatePicker.tsx
@@ -1,6 +1,7 @@
 // Imports
 import { StylableFC } from "@/utils/types/common";
 import Text from "@/components/common/Text";
+import MaterialIcon from "./MaterialIcon";
 
 const DatePicker: StylableFC<{
   name: string;
@@ -9,17 +10,25 @@ const DatePicker: StylableFC<{
   setDate: (date: string) => void;
 }> = ({ name, label, date, setDate }) => {
   return (
-    <div className="flex flex-1 flex-col items-start gap-0.5">
+    <div className="flex w-full flex-1 flex-col items-start gap-0.5 text-right">
       <Text type="body">{label}</Text>
-      <input
-        name={name}
-        type="date"
-        value={date}
-        onChange={(event) => setDate(event.target.value)}
-        className="self-strech border-primary-border text-tertiary flex
-          items-center gap-2.5 rounded-sm border py-1 pr-1 pl-2 text-sm
-          leading-[120%] font-normal"
-      />
+      <div className="relative w-full">
+        <input
+          name={name}
+          type="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+          className="border-primary-border text-tertiary flex h-8 w-full
+            appearance-none items-center gap-2.5 rounded-sm border py-1 pr-1
+            pl-2 text-sm leading-[120%] font-normal"
+        />
+        <div
+          className="text-primary pointer-events-none absolute top-1/2 right-1
+            flex -translate-y-1/2 flex-col items-center"
+        >
+          <MaterialIcon icon="event_note" />
+        </div>
+      </div>
     </div>
   );
 };

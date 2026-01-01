49899d1ed06d19d5b8d4516362f0542ce6bf00fb
Nubwan <149947073+Nubboyy@users.noreply.github.com>
2026-01-01 21:17:25 +0700

    feat: translation

--- components/landing/ActivitesCardContainer.tsx
+++ components/landing/ActivitesCardContainer.tsx
@@ -2,13 +2,15 @@ import { StylableFC } from "@/utils/types/common";
 import Card from "@/components/common/Card";
 import Text from "@/components/common/Text";
 import ActivityCard from "@/components/landing/ActivityCard";
+import { useTranslations } from "next-intl";
 
 const ActivitesCardContainer: StylableFC<{
   activities: { name: string; location: string }[];
 }> = ({ activities }) => {
+  const t = useTranslations("landing")
   return (
     <div>
-      <Text type="body">กิจกรรม</Text>
+      <Text type="body">{t("activity")}</Text>
       <div className="flex flex-col gap-1">
         {activities.map((activity, i) => (
           <ActivityCard activity={activity.name} location={activity.location} key={i}/>
--- components/landing/FAQsContainer.tsx
+++ components/landing/FAQsContainer.tsx
@@ -2,13 +2,15 @@ import { StylableFC } from "@/utils/types/common";
 import Text from "@/components/common/Text";
 import FAQDropdown from "@/components/landing/FAQDropdown";
 import { Faqs } from "@/utils/types/landing";
+import { useTranslations } from "next-intl";
 
 const FAQsContainer: StylableFC<{
   faqs: Faqs[];
 }> = ({ faqs }) => {
+  const t = useTranslations("landing")
   return (
     <div>
-      <Text type="body">คำถามที่พบบ่อย</Text>
+      <Text type="body">{t("faq")}</Text>
       <div className="flex flex-col gap-1">
         {faqs.map((faq, i) => (
           <FAQDropdown question={faq.question} answer={faq.answer} key={i}/>

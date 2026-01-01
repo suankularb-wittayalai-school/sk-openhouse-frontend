df53e5dcfba7aca7a3c57a5e5ff366f1465894f4
Nubwan <149947073+Nubboyy@users.noreply.github.com>
2026-01-01 17:46:48 +0700

    fix: translation

--- components/landing/EventTitle.tsx
+++ components/landing/EventTitle.tsx
@@ -3,11 +3,11 @@ import { useTranslations } from "next-intl";
 import { FC } from "react";
 
 const EventTitle:FC = () => {
-  const t = useTranslations();
+  const t = useTranslations("landing");
   return (
     <div className="my-8">
         <div className="flex w-full flex-col items-center justify-center">
-          <Text type="headline" className="m-1 text-center text-[32px]">
+          <Text type="headline" className="m-1 text-center text-[32px] tracking-[-0.03em]">
             <span>{t("title.first")}</span>
             <br />
             <span>{t("title.second")}</span>

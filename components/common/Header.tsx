c2f77849084cdcb46f324babc466b9f83bfd7a63
pixelpxed <pixel.pxed@gmail.com>
2026-01-01 14:48:12 +0700

    fix: header sizing improvements

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -11,31 +11,50 @@ const Header: FC = () => {
   const isLogin = false; // change later when auth is ready
 
   return (
-    <div className="flex items-center justify-between">
+    <div
+      className="max-w-content-max mb-6 flex w-screen items-center
+        justify-between overflow-auto p-2 px-3 pb-0"
+    >
       <Link href="/" className="shrink-0">
         <Image
           src={"/icons/OPH_logo.png"}
           alt="Open House Logo"
-          width={48}
-          height={48}
+          width={64}
+          height={64}
         />
       </Link>
 
-      <div className="flex gap-1">
+      <div className="flex items-center gap-1">
         {isLogin ? (
-          <div
-            className="bg-primary-surface flex h-8 w-8 items-center
-              justify-center rounded-full"
-          >
-            <MaterialIcon icon="face" size={24} />
-          </div>
+          <>
+            <Link href="/">
+              <Button variant="transparent" className="text-nowrap">
+                {t("header.details")}
+              </Button>
+            </Link>
+            <Link href="/me">
+              <Button variant="primary" className="text-nowrap">
+                {t("header.myregistration")}
+              </Button>
+            </Link>
+            <div
+              className="bg-primary-surface border-primary-border flex h-10 w-10
+                items-center justify-center rounded-full border"
+            >
+              <MaterialIcon icon="face" size={24} className="text-primary" />
+            </div>
+          </>
         ) : (
           <>
             <Link href="/login">
-              <Button variant="transparent">{t("header.login")}</Button>
+              <Button variant="transparent" className="text-nowrap">
+                {t("header.login")}
+              </Button>
             </Link>
             <Link href="/register">
-              <Button variant="primary">{t("header.register")}</Button>
+              <Button variant="primary" className="text-nowrap">
+                {t("header.register")}
+              </Button>
             </Link>
           </>
         )}

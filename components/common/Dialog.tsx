73f36dbb77a9b308a4354282ce8d6a33167aaa71
Sun-52 <iamsun2552@gmail.com>
2026-01-02 19:14:55 +0700

    feat: remove close button and add backdrop styling to Dialog component

--- components/common/Dialog.tsx
+++ components/common/Dialog.tsx
@@ -1,40 +1,27 @@
 import { StylableFC } from "@/utils/types/common";
 import { useEffect, useRef } from "react";
-import Button from "@/components/common/Button";
 import Card from "@/components/common/Card";
-import { useTranslations } from "next-intl";
 import cn from "@/utils/helpers/cn";
 
 const Dialog: StylableFC<{
   open: boolean;
-  onClose: () => void;
   children: React.ReactNode;
-}> = ({ open, onClose, children, style, className }) => {
+}> = ({ open, children, style, className }) => {
   const dialogRef = useRef<HTMLDialogElement>(null);
   useEffect(() => {
     console.log("activated");
     if (dialogRef.current?.open && !open) {
       dialogRef.current.close();
     } else if (!dialogRef.current?.open && open) {
-      dialogRef.current?.showModal();
+      dialogRef.current?.show();
     }
   }, [open]);
-  const t = useTranslations("common");
-
-  useEffect(() => {
-    const bigDialog = document.getElementById("bigDialog");
-    bigDialog?.addEventListener("click", () => onClose());
-    const card = document.getElementById("card");
-    card?.addEventListener("click", (event) => event.stopPropagation());
-    const closeButton = document.getElementById("closeButton");
-    closeButton?.addEventListener("click", () => onClose());
-  });
 
   return (
-    <dialog ref={dialogRef} id="bigDialog">
+    <dialog ref={dialogRef}>
       <div
-        className="h-vh w-vh fixed top-0 right-0 bottom-0 left-0 flex flex-col
-          items-center justify-center"
+        className="h-vh w-vh bg-on-surface-variant fixed top-0 right-0 bottom-0
+          left-0 flex flex-col items-center justify-center"
       >
         <div id="card">
           <Card
@@ -42,11 +29,6 @@ const Dialog: StylableFC<{
             className={cn("P-6 flex w-[23.125rem] flex-col gap-4", className)}
           >
             {children}
-            <div id="closeButton" className="flex flex-col">
-              <Button variant="primary" onClick={() => onClose()}>
-                {t("action.close")}
-              </Button>
-            </div>
           </Card>
         </div>
       </div>

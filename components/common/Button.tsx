41cd49dc4ed3117f3f279f6dba5ceaba25bedac7
pixelpxed <pixel.pxed@gmail.com>
2026-01-03 04:13:08 +0700

    feat: busy states for <Button>

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -3,16 +3,20 @@ import cn from "@/utils/helpers/cn";
 import { StylableFC } from "@/utils/types/common";
 
 const Button: StylableFC<{
-  variant: "primary" | "primarySurface" | "transparent"| "outline";
+  variant: "primary" | "primarySurface" | "transparent" | "outline";
   children?: React.ReactNode;
   icon?: string;
   disabled?: boolean;
+  busy?: boolean;
+  busyWithText?: boolean;
   onClick?: () => void;
 }> = ({
   variant,
   children,
   icon,
   disabled = false,
+  busy = false,
+  busyWithText = false,
   onClick,
   style,
   className,
@@ -32,6 +36,7 @@ const Button: StylableFC<{
         `flex h-10 cursor-pointer flex-col items-center justify-center gap-2
         self-stretch rounded-full transition-all`,
         BUTTON_VARIANT[variant],
+        (disabled || busy) && "pointer-events-none brightness-60",
         className,
       )}
       disabled={disabled}
@@ -41,10 +46,20 @@ const Button: StylableFC<{
         className={cn(
           `flex flex-1 items-center justify-center gap-2 self-stretch px-6
           py-2.5 text-sm leading-[140%] font-normal`,
+          icon || busy && "pl-4.5!"
         )}
       >
-        {icon && <MaterialIcon icon={icon} />}
-        {children}
+        {busy && (
+          <div className="grid place-items-center">
+            <MaterialIcon icon="progress_activity" className="animate-spin" />
+          </div>
+        )}
+        {!(busy && !busyWithText) && (
+          <>
+            {icon && <MaterialIcon icon={icon} />}
+            {children}
+          </>
+        )}
       </div>
     </button>
   );

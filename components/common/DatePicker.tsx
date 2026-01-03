194f836eaec9da32c964392f8f781dda44190581
pixelpxed <pixel.pxed@gmail.com>
2026-01-04 04:34:33 +0700

    fix: datepicker doesn't fill the whole width

--- components/common/DatePicker.tsx
+++ components/common/DatePicker.tsx
@@ -12,17 +12,15 @@ const DatePicker: StylableFC<{
   return (
     <div className="flex w-full flex-1 flex-col items-start gap-0.5 text-right">
       <Text type="body">{label}</Text>
-      <div className="w-full">
-        <input
-          name={name}
-          type="date"
-          value={date}
-          onChange={(event) => setDate(event.target.value)}
-          className="border-primary-border text-tertiary flex h-8 w-full
-            items-center gap-2.5 rounded-sm border py-1 pr-1 pl-2 text-sm
-            leading-[120%] font-normal"
-        />
-      </div>
+      <input
+        name={name}
+        type="date"
+        value={date}
+        onChange={(event) => setDate(event.target.value)}
+        className="border-primary-border text-tertiary h-8 w-full
+          items-center gap-2.5 rounded-sm border py-1 pr-1 pl-2 text-sm
+          leading-[120%] font-normal"
+      />
     </div>
   );
 };

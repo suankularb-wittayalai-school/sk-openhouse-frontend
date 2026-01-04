15caeacde5c6dae727a3b6c1d42f6069e35f8bb7
pixelpxed <pixel.pxed@gmail.com>
2026-01-04 15:49:17 +0700

    fix: in development environments headers drop everything for auth token only

--- utils/helpers/fetchAPI.ts
+++ utils/helpers/fetchAPI.ts
@@ -1,13 +1,18 @@
 async function fetchAPI(url: string, options: RequestInit) {
+  const headers = new Headers(options.headers);
+
   let token: string = "";
   if (typeof window !== "undefined") {
     token = localStorage.getItem("skopen26-sessionToken") ?? "null";
   }
 
-  // Handle different auth methods for different environments.
-  process.env.NODE_ENV == "development"
-    ? (options.headers = { Authorization: `Bearer ${token}` })
-    : (options.credentials = "include");
+  if (process.env.NODE_ENV === "development") {
+    headers.set("Authorization", `Bearer ${token}`);
+  } else {
+    options.credentials = "include";
+  }
+
+  options.headers = headers;
 
   return await fetch(
     `${process.env.NEXT_PUBLIC_OPENHOUSE_API_URL}${url}`,

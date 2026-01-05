c8ee31ed45ee6c2a85da8e6f6870852ea10e4f1b
pixelpxed <pixel.pxed@gmail.com>
2026-01-06 02:10:09 +0700

    feat: new landing (unfinished)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
-import { getStaticTranslations } from "@/utils/helpers/getStaticTranslations";
-import Button from "@/components/common/Button";
-import EventTitle from "@/components/landing/EventTitle";
-import { GetStaticProps } from "next";
-import Link from "next/link";
-import ActivitesCardContainer from "@/components/landing/ActivitesSection";
-import FAQsContainer from "@/components/landing/FAQSection";
-import { FC } from "react";
-import { ActivitiesList, BusRoute, Faqs } from "@/utils/types/landing";
-import { useTranslations } from "next-intl";
-import TransitGuideMRTSection from "@/components/landing/TransitGuideMRTSection";
-import TransitGuideBusSection from "@/components/landing/TransitGuideBusSection";
-import MapsSchoolLocation from "@/components/landing/subcomponents/MapsSchoolLocation";
+import CardContainer from "@/components/common/CardContainer";
 import Text from "@/components/common/Text";
+import EventTitle from "@/components/landing/EventTitle";
+import FAQsContainer from "@/components/landing/FAQSection";
+import TransitGuideBusSection from "@/components/landing/TransitGuideBusSection";
+import TransitGuideMRTSection from "@/components/landing/TransitGuideMRTSection";
+import MapsSchoolLocation from "@/components/landing/subcomponents/MapsSchoolLocation";
 import ScheduleCard from "@/components/landing/subcomponents/ScheduleCard";
 import SchoolMap from "@/components/me/SchoolMap";
 import fetchAPI from "@/utils/helpers/fetchAPI";
+import { getStaticTranslations } from "@/utils/helpers/getStaticTranslations";
+import { ActivitiesList, BusRoute, Faqs } from "@/utils/types/landing";
+import { scheduleItem } from "@/utils/types/schedule";
+import { GetStaticProps } from "next";
+import { useTranslations } from "next-intl";
+import { FC } from "react";
 
 const LandingPage: FC<{
   activities: ActivitiesList[];
@@ -62,18 +61,53 @@ export const getStaticProps: GetStaticProps = async () => {
   const messages = await getStaticTranslations("common", "landing");
 
   /* Tempoary !!!  */
+  // Lmao it's not temp now
   const faqs: Faqs[] = [
     {
-      question: "ต้องแต่งชุดอะไรไป ?",
-      answer: "อยากใส่ชุดอะไรก็ใส่เลยครับ",
+      question: "ลงทะเบียนเข้าร่วมงาน SK Open House 2026 อย่างไร",
+      answer:
+        "ผู้ปกครองกดปุ่มลงทะเบียนผ่านเว็บไซต์ http://openhouse.mysk.school ด้วยบัญชี Google (ไม่สามารถใช้บัญชี @sk.ac.th หรือ @student.sk.ac.th สำหรับการลงทะเบียนได้) โดยผู้ปกครองจะเป็นเจ้าของบัญชีครอบครัว จากนั้น ผู้ปกครองสามารถเพิ่มสมาชิกที่จะเข้าร่วมงาน SK Open House 2026 เช่น ผู้ปกครองอีกท่าน หรือนักเรียนได้ครับ",
     },
     {
-      question: "คำถามอื่น ?",
-      answer: "อยากใส่ชุดอะไรก็ใส่เลยครับ",
+      question: "จำเป็นต้องเพิ่มนักเรียนเป็นสมาชิกในบัญชีครอบครัวหรือไม่",
+      answer:
+        "เมื่อเพิ่มนักเรียนเป็นสมาชิกในบัญชีครอบครัว จะมี QR code เพื่อให้นักเรียนสะสมคะแนนจากการเข้าร่วมกิจกรรมในโซนนิทรรศการ และนำไปแลกของรางวัลได้ครับ",
     },
     {
-      question: "คำถามนอกโลกไปแล้ว ?",
-      answer: "อยากใส่ชุดอะไรก็ใส่เลยครับ",
+      question: "งาน SK Open House 2026 จัดที่ไหน และเมื่อใด",
+      answer:
+        "งาน SK Open House 2026 จัดขึ้นที่โรงเรียนสวนกุหลาบวิทยาลัย ถนนตรีเพชร เขตพระนคร กรุงเทพฯ​ วันที่ 11 มกราคม 2569 เริ่มงานเวลา 8.00 เป็นต้นไปครับ",
+    },
+    {
+      question: "งาน SK Open House 2026 มีกิจกรรมอะไรบ้าง",
+      answer: `ภายในงานจะมีกิจกรรม ดังนี้ 1. การนำเสนอข้อมูลหลักสูตรของแต่ละโครงการฯ ของโรงเรียนสวนกุหลาบวิทยาลัย ณ หอประชุมสวนกุหลาบรำลึก โดยตัวแทนของแต่ละหลักสูตร พร้อมการตอบคำถามที่ผู้ปกครองได้กรอกมาในการลงทะเบียน 2. นิทรรศการ SK Open House ในบริเวณต่างๆ ของโรงเรียน ได้แก่ โซนนิทรรศการ ใต้อาคาร 123 ปี สวนกุหลาบวิทยาลัยเกี่ยวกับหลักสูตรปกติ, GATE Program, Gifted Science, EPLUS+ และหลักสูตรใหม่ "วิทยาศาสตร์หุ่นยนต์" อีกทั้งยังมีการให้ข้อมูลเกี่ยวกับการรับนักเรียน ความสำเร็จของโรงเรียนในปีที่ผ่านมา นิทรรศการจากครูแนะแนวและความร่วมมือทางวิชาการ นอกจากนี้ ยังมีกิจกรรม "ตึกยาวทรงคุณค่า" ณ บริเวณพิพิธภัณฑ์ตึกยาว, "SK Sports Challenge" ณ บริเวณหน้าทางเดินตึกยาว และ "ท้องฟ้าจำลองสิรินธร"`,
+    },
+    {
+      question: "มางาน SK Open House 2026 ต้องแต่งกายอย่างไร",
+      answer: "นักเรียนและผู้ปกครองแต่งกายชุดสุภาพเพื่อเข้าร่วมกิจกรรมครับ",
+    },
+    {
+      question:
+        "หากมางาน SK Open House 2026 ไม่ได้ จะติดตามข้อมูลเกี่ยวกับโรงเรียนและการรับสมัครนักเรียนได้อย่างไร",
+      answer:
+        "ผู้ปกครองและนักเรียนสามารถติดตามข่าวสารเกี่ยวกับโรงเรียนและการรับสมัครนักเรียนได้จาก https://www.facebook.com/PRsuankularb ครับ",
+    },
+    {
+      question: "มีกิจกรรมพิเศษสำหรับนักเรียนหรือไม่",
+      answer: `นอกจากการบรรยายหลักสูตร โรงเรียนได้จัดกิจกรรมเพื่อสร้างความเข้าใจเกี่ยวกับโรงเรียนและหลักสูตร โดยครูและรุ่นพี่สวนกุหลาบฯ และมีการสะสมคะแนนสำหรับนักเรียน จำนวน 10 ฐานกิจกรรม ดังนี้ครับ
+
+ฐานที่ 1 – บูธนิทรรศการข้อมูลการรับนักเรียน และหลักสูตรโรงเรียนสวนกุหลาบวิทยาลัย
+ฐานที่ 2 – บูธนิทรรศการ “Year in Review” : บันทึกความสําเร็จของเด็กสวนฯ
+ฐานที่ 3 - บูธนิทรรศการห้องเรียนวิทยาศาสตร์หุ่นยนต์ Robotic Science
+ฐานที่ 4 - บูธนิทรรศการความร่วมมือทางด้านวิชาการในประเทศและต่างประเทศ
+ฐานที่ 5 - บูธนิทรรศการโครงการห้องเรียนพิเศษ EPLUS+
+ฐานที่ 6 - บูธนิทรรศการโครงการห้องเรียนพิเศษ GATE Program
+ฐานที่ 7 - บูธนิทรรศการโครงการห้องเรียนพิเศษ Gifted Science
+ฐานที่ 8 – SK Sports Challenge
+ฐานที่ 9 - ตึกยาวทรงคุณค่า
+ฐานที่ 10 – ท้องฟ้าจําลองสิรินธร
+
+เมื่อนักเรียนสะสมคะแนนครบตามที่กำหนด สามารถแลกรับของรางวัลพิเศษจากทางโรงเรียนได้ครับ`,
     },
   ];
 

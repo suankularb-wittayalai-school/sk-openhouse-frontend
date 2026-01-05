684cb6fd5450d7e172e38bfbe37b9522647d86a5
pixelpxed <pixel.pxed@gmail.com>
2026-01-06 02:08:59 +0700

    feat: schedule card separating content details with chips (unfinished for location only items)

--- components/landing/subcomponents/ScheduleCard.tsx
+++ components/landing/subcomponents/ScheduleCard.tsx
@@ -14,15 +14,39 @@ const ScheduleCard: StylableFC<{ scheduleItem: scheduleItem }> = ({
   return (
     <Card>
       <div className="flex flex-col">
-        <Text type="title">{scheduleItem.name}</Text>
-        <Text type="body">
-          {scheduleItem.start_time.slice(0, 5) +
-            " - " +
-            scheduleItem.end_time.slice(0, 5) +
-            " น." +
-            " • " +
-            scheduleItem.description}
+        <Text type="title" className="font-bold!">
+          {scheduleItem.name}
         </Text>
+        <div className="mt-1 flex flex-col gap-1">
+          <Text type="title" className="text-tertiary!">
+            {scheduleItem.description.split(" ณ ").length == 1
+                  ? ""
+                  : scheduleItem.description.split(" ณ ")[0]}
+          </Text>
+          <div className="flex gap-1">
+            <div
+              className="border-primary-border w-full max-w-max rounded-full
+                border px-2 py-0.5"
+            >
+              <Text type="title" className="text-tertiary!">
+                {scheduleItem.start_time.slice(0, 5) +
+                  " - " +
+                  scheduleItem.end_time.slice(0, 5) +
+                  " น."}
+              </Text>
+            </div>
+            <div
+              className="border-primary-border w-full max-w-max rounded-full
+                border px-2 py-0.5"
+            >
+              <Text type="title" className="text-tertiary!">
+                {scheduleItem.description.split(" ณ ").length == 1
+                  ? scheduleItem.description.split(" ณ ")[0]
+                  : scheduleItem.description.split(" ณ ")[1]}
+              </Text>
+            </div>
+          </div>
+        </div>
       </div>
     </Card>
   );

fe8fff6520f830f97bee33ada83d620d224199da
pixelpxed <pixel.pxed@gmail.com>
2026-01-05 22:47:48 +0700

    feat: heading title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>SK Openhouse 2026</title>
         <script src="https://accounts.google.com/gsi/client" async />
       </Head>
       <body className="antialiased">

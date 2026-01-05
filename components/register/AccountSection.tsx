4a22d3391701e80ca3176c1276ce5c7f3dfe503f
pixelpxed <pixel.pxed@gmail.com>
2026-01-06 03:47:14 +0700

    feat: removing devcards

--- components/register/AccountSection.tsx
+++ components/register/AccountSection.tsx
@@ -15,10 +15,9 @@ const AccountSection: FC<{
   type?: "register" | "signIn";
   onRedirect: () => void;
 }> = ({ type = "register", onRedirect }) => {
-  const [state, setState] = useState<GSIStatus>(GSIStatus.initial);
-  const [token, setToken] = useState<string>("");
-  const [testResponse, setTestResponse] = useState<string>("");
   const t = useTranslations("register");
+
+  const [state, setState] = useState<GSIStatus>(GSIStatus.initial);
 
   useEffect(() => {
     if (state == GSIStatus.redirecting) {
@@ -44,11 +43,7 @@ const AccountSection: FC<{
           >
             {
               {
-                [GSIStatus.initial]: (
-                  <GSIButton
-                    onStateChange={setState}
-                  />
-                ),
+                [GSIStatus.initial]: <GSIButton onStateChange={setState} />,
                 [GSIStatus.chooserShown]: (
                   <>
                     <Text type="title">
@@ -73,90 +68,6 @@ const AccountSection: FC<{
           </motion.div>
         </AnimatePresence>
       </div>
-      {process.env.NODE_ENV == "development" && (
-        <Card className="flex flex-col gap-2">
-          <div className="flex flex-col">
-            <p className="text-lg font-bold">DEV: Signing in via OAUTH2.0</p>
-            <p className="text-sm">
-              This method is only available in development builds.
-            </p>
-          </div>
-          <div>
-            <p className="mb-2 text-sm">
-              1. Get <b>auth_token</b> from backend:
-            </p>
-            <Link
-              target="_blank"
-              href={
-                process.env.NEXT_PUBLIC_OPENHOUSE_API_URL +
-                "/v1/user/oauth/init"
-              }
-            >
-              <Button variant="primary" className="w-full">
-                Sign in with OAUTH2.0
-              </Button>
-            </Link>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p className="text-sm">
-              2. Paste <b>auth_token</b> from backend onto the field below,
-              submit, then go on your day.
-              <br />
-              (You may test if it's functioning by using the <b>
-                testFetch()
-              </b>{" "}
-              function below.):
-            </p>
-            <TextField
-              name={"token"}
-              label="Token"
-              value={token}
-              setValue={setToken}
-              className="w-full"
-            />
-            <div className="flex flex-col gap-2 sm:flex-row">
-              <div className="flex w-full flex-col gap-2">
-                <Button
-                  variant={"primary"}
-                  disabled={token == ""}
-                  onClick={() => {
-                    setToken("");
-                    if (typeof window !== "undefined") {
-                      localStorage.setItem("skopen26-sessionToken", token);
-                    }
-                  }}
-                >
-                  Set Token (localStorage)
-                </Button>
-                <Button
-                  variant={"primarySurface"}
-                  onClick={() => {
-                    setTestResponse("Fetching...");
-                    fetchAPI("/v1/user", {
-                      method: "GET",
-                    })
-                      .then((res) => res.json())
-                      .then((data) => setTestResponse(data));
-                  }}
-                >
-                  Test Fetch (/user)
-                </Button>
-              </div>
-              <div
-                className="border-primary-border w-full rounded-lg border p-2"
-              >
-                <p className="font-[monospace]! text-xs break-all">
-                  <b>GET {process.env.NEXT_PUBLIC_OPENHOUSE_API_URL}/v1/user</b>
-                  <br />
-                  {testResponse !== ""
-                    ? JSON.stringify(testResponse)
-                    : "No requests have been taken yet."}
-                </p>
-              </div>
-            </div>
-          </div>
-        </Card>
-      )}
     </div>
   );
 };
